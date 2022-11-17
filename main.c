e4ebf67818049732b673adf6e78dabc0c3c7e13d
Eatswap <root@eatswap.org>
2022-11-17 13:08:33 +0800

    fix: remove unsupported signals

--- main.c
+++ main.c
@@ -148,8 +148,6 @@ static void sighandler(int sig __UNUSED__)
 {
 	signal(sig, SIG_IGN);
 	signal(SIGTERM, SIG_IGN);
-	signal(SIGTTIN, SIG_IGN);
-	signal(SIGTTOU, SIG_IGN);
 	print_err("Interrupted\n");
 	fatal_exit(&local_control);
 }
